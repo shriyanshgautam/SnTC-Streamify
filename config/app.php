31c9397471e239b6b588d818ada6c0507dc7133b
Shriyansh Gautam <shriyanshgautam005@gmail.com>
2017-08-26 04:43:59 +0530

    guzzle and symphony psr7 added for rest api calls

--- config/app.php
+++ config/app.php
@@ -163,13 +163,15 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
 
         TCG\Voyager\VoyagerServiceProvider::class,
-
         //
+
+
 
         /*
          * Application Service Providers...
